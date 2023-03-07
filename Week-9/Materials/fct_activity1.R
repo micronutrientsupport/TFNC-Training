41dc347995a8977fc91af47b27030fc8d986138d
Lucia Segovia De La Revilla <lshls9@lshtm.ac.uk>
2023-03-07 12:38:42 +0000

    added pdf of presentation and fixed data paths

--- Week-9/Materials/fct_activity1.R
+++ Week-9/Materials/fct_activity1.R
@@ -1,32 +1,47 @@
 
 
-# Welcome to Week 9! 
-
-
 # Loading libraries
+library(tidyverse)
 
 
 # Loading the data
-
+df <- read.csv("Week-9/Materials/data/TZ08_FCT_Partial.csv")
 
 # Checking the loaded data
-
 ## How many rows has the data?
 
+nrow(df)
+
+message(paste0("This dataset has ", nrow(df), " rows of data"))
 
 ## How many columns has the data?
+
+length(df)
+
+ncol(df)
+
+message(paste0("This dataset has ", ncol(df), " rows of data"))
 
 
 ## What are the variables names? 
 
+names(df)
+
+colnames(df)
 
 # Tidying the data
 
-## 0) Visually checking the data
-
-
+## Visually checking the data
+head(df)
+tail(df)
+View(df)
 
 ## 1) Trimming the dataframe vertically
+
+df <- df %>% slice(c(1:378)) #This selects only the rows 1:10 to keep. 
+#We can do a number of selections like this; e.g. slice(c(1:4, 6:10)) 
+#would keep rows 1 to 4 and 6 to 10, but miss out 5, 11, 12, 13, 14.
+
 
 
 
